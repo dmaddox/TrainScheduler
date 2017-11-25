80c2421d38b2408d7a4331f6463a13f6e874e414
dmaddox <dmaddox@gmail.com>
2017-11-25 14:54:42 -0500

    added field verifications and alerting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,20 +13,80 @@
 var db = firebase.database();
 
 // declare variables
-var trainName = "";
-var destName = "";
-var firstTrain = "";
-var freq = "";
+var trainName = $("#train-name");
+var destName = $("#dest-name");
+var firstTrain = $("#first-train");
+var freq = $("#freq");
+
+
+// verify inputs
+trainName.blur(function() {
+	//if trainName is empty
+	if(trainName.val().trim() === '') {
+		// remove any existing warnings
+		$("#alert-name").remove("div");
+		// set warning message & disable submit
+		trainName.parent().append("<div class='alert alert-danger' id='alert-name' role='alert'>You must input the train's name.</div>");
+		$("#submit").attr("disabled", true);
+	} else {
+		$("#alert-name").remove();
+		$("#submit").attr("disabled", false);
+	};
+});
+destName.blur(function() {
+	//if trainName is empty
+	if(destName.val().trim() === '') {
+		// remove any existing warnings
+		$("#alert-dest").remove("div");
+		// set warning message & disable submit
+		destName.parent().append("<div class='alert alert-danger' id='alert-dest' role='alert'>You must input the train's destination.</div>");
+		$("#submit").attr("disabled", true);
+	} else {
+		$("#alert-dest").remove();
+		$("#submit").attr("disabled", false);
+	};
+});
+firstTrain.blur(function() {
+	//if firstTrain is empty or if it doesnt match military time
+	// setup military time checks
+	if(firstTrain.val().trim().match(/^([01]?[0-9]|2[0-3]):[0-5][0-9]$/) === null) {
+
+		// remove any existing warnings
+		$("#alert-first").remove("div");
+		// set warning message & disable submit
+		firstTrain.parent().append("<div class='alert alert-danger' id='alert-first' role='alert'>Input the train's first departure time in a valid military time. Ex: \"00:30\"</div>");
+		$("#submit").attr("disabled", true);
+	} else {
+		$("#alert-first").remove();
+		$("#submit").attr("disabled", false);
+	};
+});
+freq.blur(function() {
+	//if firstTrain is empty or if it doesnt match military time
+	var a = parseInt(freq.val().trim())
+	if(!Number.isInteger(a)) {
+		// remove any existing warnings
+		$("#alert-freq").remove("div");
+		// set warning message & disable submit
+		freq.parent().append("<div class='alert alert-danger' id='alert-freq' role='alert'>Input the train's frequency of departure in minutes. Ex: \"45\"</div>");
+		$("#submit").attr("disabled", true);
+	} else {
+		$("#alert-freq").remove();
+		$("#submit").attr("disabled", false);
+	};
+});
+
 
 // when user hits submit, retrieve all input information
 $("#submit").on("click", function(event) {
 	// prevent submit from refreshing page
 	event.preventDefault()
+
 	// initialize variables with field inputs
-	trainName = $("#train-name").val().trim();
-	destName = $("#dest-name").val().trim();
-	firstTrain = $("#first-train").val().trim();
-	freq = $("#freq").val().trim();
+	trainName = trainName.val().trim();
+	destName = destName.val().trim();
+	firstTrain = firstTrain.val().trim();
+	freq = freq.val().trim();
 
 	// new object to store all train info
 	var trains = {
@@ -45,9 +105,6 @@ $("#submit").on("click", function(event) {
 	$("#first-train").val("");
 	$("#freq").val("");
 })
-
-
-
 
 // when the database updates, update the screen array
 db.ref().on("child_added", function (childSnapshot, prevChildKey) {
@@ -80,6 +137,7 @@ db.ref().on("child_added", function (childSnapshot, prevChildKey) {
 	// Add db data to html table
 	$("#train-table > tbody").append("<tr><td>" + dbName + "</td><td>" + dbDest + "</td><td>" + dbFreq + "</td><td>" + dbFirst + "</td><td>" + timeLeft + "</td></tr>");
 })
+
 
 
 
